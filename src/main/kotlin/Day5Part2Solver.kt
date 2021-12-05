65f72c9eb69fe72dd290a8dfb8554b46ddd7d473
Alexey Plutalov <demiazz.py@gmail.com>
2021-12-05 19:59:34 +0300

    replace iterator with sequence

--- src/main/kotlin/Day5Part2Solver.kt
+++ src/main/kotlin/Day5Part2Solver.kt
@@ -1,22 +1,6 @@
 object Day5Part2Solver : Solver {
     private val pattern = Regex("(\\d+),(\\d+) -> (\\d+),(\\d+)")
 
-    private class CoordinatesIterator(var x: Int, var y: Int, val stopX: Int, val stopY: Int): Iterator<Pair<Int, Int>> {
-        val stepX = if (x < stopX) 1 else -1
-        val stepY = if (y < stopY) 1 else -1
-
-        override fun hasNext(): Boolean = x != stopX && y != stopY
-
-        override fun next(): Pair<Int, Int> {
-            val point = Pair(x, y)
-
-            x += stepX
-            y += stepY
-
-            return point
-        }
-    }
-
     private class Section(val x1: Int, val y1: Int, val x2: Int, val y2: Int) {
         fun apply(points: MutableMap<String, Int>): MutableMap<String, Int> {
             val coordinates =
@@ -24,14 +8,29 @@ object Day5Part2Solver : Solver {
                     val fromY = y1.coerceAtMost(y2)
                     val toY = y1.coerceAtLeast(y2)
 
-                    (fromY..toY).map { Pair(x1, it) }.iterator()
+                    (fromY..toY).asSequence().map { Pair(x1, it) }
                 } else if (y1 == y2) {
                     val fromX = x1.coerceAtMost(x2)
                     val toX = x1.coerceAtLeast(x2)
 
-                    (fromX..toX).map { Pair(it, y1) }.iterator()
+                    (fromX..toX).asSequence().map { Pair(it, y1) }
                 } else {
-                    CoordinatesIterator(x1, y1, x2, y2)
+                    sequence<Pair<Int, Int>> {
+                        var x = x1
+                        var y = y1
+
+                        val stepX = if (x1 < x2) 1 else -1
+                        val stepY = if (y1 < y2) 1 else -1
+
+                        while (x != x2 && y != y2) {
+                            val point = Pair(x, y)
+
+                            yield(point)
+
+                            x += stepX
+                            y += stepY
+                        }
+                    }
                 }
 
             for ((x, y) in coordinates) {
